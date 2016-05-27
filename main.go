7594ccfe974b9e1f9bc418f5fe159e58e59cfe24
Ardeshir Sepahsalar <asepahsalar@hennepintech.edu>
2016-05-27 20:08:49 +0000

    added client & room

--- main.go
+++ main.go
@@ -1,35 +1,41 @@
 package main
 
-import(
-    "log"
-     "net/http"
-     "text/template"
-     "path/filepath"
-     "sync"
-    )
-    
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+	"flag"
+)
+
 // struck to manage templates
 type templateHandler struct {
-   once  sync.Once
-   filename string
-   templ    *template.Template
+	once     sync.Once
+	filename string
+	templ    *template.Template
 }
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-        t.once.Do(func() {
-              t.templ = 
-              template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
-        })
-        t.templ.Execute(w,nil)
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
 }
-    
+
 func main() {
 
-http.Handle("/", &templateHandler{ filename: "index.html"})
-http.Handle("/about", &templateHandler{ filename: "about.html"})
+	var port = flag.String("port", ":8090", "The port addr of the app.")
+	flag.Parse()
 
-// start the web server
-    if err := http.ListenAndServe(":80", nil); err != nil {
-            log.Fatal("ListenAndServe:", err)
-    }
+	http.Handle("/", &templateHandler{filename: "index.html"})
+	http.Handle("/about", &templateHandler{filename: "about.html"})
+
+	log.Println("Starting web server on ", *port)
+
+	// start the web server
+	if err := http.ListenAndServe(*port, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
